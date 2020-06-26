b3e4f487d1906c9f7601d0af39c572e18e4dc0d2
sbsteacher <pirbak@daum.net>
2020-06-26 15:23:22 +0900

    first

---
+++ board3/WebContent/WEB-INF/jsp/login.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" type="text/css" href="/css/common.css">
+<link rel="stylesheet" type="text/css" href="/css/login.css">
+</head>
+<body>
+	<div id="container">
+		<div id="loginForm">
+			<div><input type="text" name="cid" placeholder="id"></div>
+			<div><input type="password" name="cid" placeholder="password"></div>
+			<div><input type="submit" value="Login"></div>
+			<div><a href="/join">회원가입</a></div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ board3/src/com/koreait/board3/LoginSer.java
@@ -0,0 +1,23 @@
+package com.koreait.board3;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginSer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String jsp = "/WEB-INF/jsp/login.jsp";
+		request.getRequestDispatcher(jsp).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
