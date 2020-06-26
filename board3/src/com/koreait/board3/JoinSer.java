93d868aa7c463eed6d3855e3f1f07d3fcdc26828
sbsteacher <pirbak@daum.net>
2020-06-26 16:39:52 +0900

    조인 작업중

--- board3/src/com/koreait/board3/JoinSer.java
+++ board3/src/com/koreait/board3/JoinSer.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.koreait.board3.dao.UserDAO;
+import com.koreait.board3.vo.UserVO;
+
 @WebServlet("/join")
 public class JoinSer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -17,7 +20,20 @@ public class JoinSer extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String cid = request.getParameter("cid");
+		String cpw = request.getParameter("cpw");
+		String nm = request.getParameter("nm");
 		
+		System.out.println("cid: " + cid);
+		System.out.println("cpw: " + cpw);
+		System.out.println("nm: " + nm);
+				
+		UserVO param = new UserVO();
+		param.setCid(cid);
+		param.setCpw(cpw);
+		param.setNm(nm);
+		
+		int result = UserDAO.join(param);
 	}
 
 }
---
+++ board3/src/com/koreait/board3/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package com.koreait.board3.dao;
+
+import com.koreait.board3.vo.*;
+
+public class UserDAO {
+	//로그인성공: 1, 아이디없음: 2,  비밀번호 틀림: 3, 에러: 0
+	public static int join(UserVO param) {
+		int result = 0;
+		
+		
+		
+		return result;
+	}
+}
